e3fe509ae241806832a03cf3dacc0ca60eaad553
Daniel Visoiu <daniel.visoiu@toptal.com>
2021-03-08 20:51:54 +0200

    Added data-if and data-for usages in webcardinal forms.

--- scripts/controllers/webcardinal/WebcFormsController.js
+++ scripts/controllers/webcardinal/WebcFormsController.js
@@ -19,12 +19,30 @@ const model = {
             placeholder: "Address 2",
         },
     ],
+    options: [
+        { value: "value1", text: "Text 1" },
+        { value: "value2", text: "Text 2" },
+        { value: "value3", text: "Text 3" },
+    ],
+    terms: {
+        type: "checkbox",
+        placeholder: "terms",
+        checked: true,
+    },
+    selectedOption: "value2",
 };
 
 export default class WebcFormsController extends WebcController {
     constructor(element, history) {
         super(element, history);
         this.setModel(JSON.parse(JSON.stringify(model)));
+
+        this.onTagClick("add-address", () => {
+            this.model.addresses.push({
+                name: `address${this.model.addresses.length + 1}`,
+                placeholder: "Extra address",
+            });
+        });
 
         this.onTagClick("submit", () => {
             const formData = this.model.toObject("name");
@@ -36,7 +54,15 @@ export default class WebcFormsController extends WebcController {
             () => JSON.stringify(this.model.toObject(), null, 2),
             "name",
             "addresses",
-            "description"
+            "description",
+            "selectedOption",
+            "terms"
         );
+
+        this.model.addExpression("canAddAddresses", () => this.model.addresses.length < 4, "addresses");
+
+        setTimeout(() => {
+            this.model.selectedOption = "value3";
+        }, 2000);
     }
 }
